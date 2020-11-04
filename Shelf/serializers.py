1b82f6623f1cfe959982c96088a74a0f15754fd2
Daniel Lotsman <ddaannttee1155@gmail.com>
2020-11-04 17:43:44 +0200

    Added views,serializers and urls to get books and authors

--- Shelf/serializers.py
+++ Shelf/serializers.py
@@ -12,12 +12,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    status = serializers.IntegerField()
+    priority = serializers.IntegerField()
+    description = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    owner = serializers.CharField()
+    pub_year = serializers.DateField()
+    author = serializers.CharField()
+
     class Meta:
         model = BookObject
-        fields = ("id", "title", "pub_year", "author")
+        fields = ("id", "title", "pub_year", "author","status","priority","description","created_at","owner")
 
 
 class BookAuthorSerializer(serializers.ModelSerializer):
+    title = serializers.CharField()
+    description = serializers.CharField()
+    born = serializers.DateField()
+    died = serializers.DateField()
+
     class Meta:
         model = BookAuthor
-        fields = ("id", "title", "born")
+        fields = ("id", "title", "description", "born", "died")
